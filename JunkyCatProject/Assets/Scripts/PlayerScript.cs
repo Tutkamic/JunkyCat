c0402cc6e6b106b3a8142316ac04d76fdae08243
Tutkamic <michal.tutka90@gmail.com>
2022-05-22 19:16:56 +0200

    Add movement

--- JunkyCatProject/Assets/Scripts/PlayerScript.cs
+++ JunkyCatProject/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,10 @@ public class PlayerScript : MonoBehaviour
     public Material catMaterial;
     public Texture defaultTexture;
 
+    [SerializeField] private Rigidbody playerRigidBody;
+    private Joystick joystick;
+    [SerializeField] private float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +18,27 @@ public class PlayerScript : MonoBehaviour
             catMaterial.SetTexture("_BaseMap", GameManagerScript.instance.playerMaterialTexture);
         else
             catMaterial.SetTexture("_BaseMap", defaultTexture);
+
+        joystick = FindObjectOfType<Joystick>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(joystick != null)
+        {
+            playerRigidBody.velocity = new Vector3(joystick.Horizontal * moveSpeed, playerRigidBody.velocity.y * (-1), joystick.Vertical * moveSpeed);
+
+            if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+            {
+                transform.rotation = Quaternion.LookRotation(playerRigidBody.velocity * (-1));
+            }
+        }
 
     }
 
